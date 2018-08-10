b86d7c75d85a88e592c52a7d2d2c49428f65237a
UmaHani <umahanimohammadi@yahoo.com>
2018-08-10 16:49:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # dog.rb
 class Dog 
   def name=(dog_name)
-    this_dogs_name = dog_name 
+    @this_dogs_name = dog_name 
   end 
-  def name this_dogs_name
+  def name @this_dogs_name
   end 
   lassie = Dog.new 
   lassie.name = "Lassie"
-  lassie.name 
+  puts lassie.name 
 end 
